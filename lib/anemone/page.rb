15fdf61e76cf20f8b4127306b6d2e3690cb49871
xiaohui <xiaohui@zhangxh.net>
2015-08-25 10:45:15 +0800

    remove # from end of URL

--- lib/anemone/page.rb
+++ lib/anemone/page.rb
@@ -156,7 +156,9 @@ module Anemone
     def base
       @base = if doc
         href = doc.search('//head/base/@href')
-        URI(href.to_s) unless href.nil? rescue nil
+        b = URI(href.to_s) unless href.nil? rescue nil
+        b = @url.merge(b) if b && b.relative?
+        b
       end unless @base
       
       return nil if @base && @base.to_s().empty?
@@ -174,11 +176,11 @@ module Anemone
       # remove anchor
       # comment this line since there are some Ajax website use anchor
       # link = URI.encode(URI.decode(link.to_s.gsub(/#[a-zA-Z0-9_-]*$/,'')))
+      link = link.gsub(/#+$/, '')
       link = link.gsub(/[{}]/, '')
 
       relative = URI(link)
-      base_uri = base.absolute? ? base : @url.merge(base) if base
-      absolute = base_uri ? base_uri.merge(relative) : @url.merge(relative)
+      absolute = base ? base.merge(relative) : @url.merge(relative)
 
       absolute.path = '/' if absolute.path.blank?
 
